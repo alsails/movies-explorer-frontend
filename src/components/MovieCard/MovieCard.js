a70285908169c2feecda5462ab544a20d207df5b
Your Name <mash.pogonina2014@yandex.ru>
2023-07-02 20:41:06 +0300

    feat: добавить компоненты

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,12 +1,13 @@
 import './MovieCard.css';
 
 function MovieCard({movie}) {
+  const isLike = movie.likes;
   return (
     <div className="moviesCard">
         <img className='moviesCard__img' src={movie.img} alt={`Обложка фильма ${movie.name}`}/>
         <div className='moviesCard__container'>
           <p className='moviesCard__title'>{movie.name}</p>
-          <button className='moviesCard__like' />
+          <button className={`moviesCard__like ${isLike ? 'moviesCard__like_active' : ''}`} />
         </div>
         <p className='moviesCard__duration'>{movie.time}</p>
     </div>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,6 +2,7 @@ import Header from "../Header/Header";
 import More from "../More/More";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import Preloader from "../Preloader/Preloader";
+import SavedMovies from "../SavedMovies/SavedMovies";
 import SearchForm from "../SearchForm/SearchForm";
 
 function Movies({isLogined}) {
@@ -10,8 +11,9 @@ function Movies({isLogined}) {
        <Header isLogined={isLogined} />
        <SearchForm />
        {/* <Preloader /> */}
-       <MoviesCardList />
-       <More />
+       {/* <MoviesCardList />
+       <More /> */}
+       <SavedMovies />
     </>
   );
 }
