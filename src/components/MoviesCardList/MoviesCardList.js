e1db973039a20e82923c7b88e41157aebe44dd1e
Your Name <mash.pogonina2014@yandex.ru>
2023-08-25 22:56:31 +0300

    fix: исправить отрисовку поределенного количества карточек в зависимости от размера экрана

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,62 +4,78 @@ import movies from "../../utils/movies";
 import MovieCard from "../MovieCard/MovieCard";
 import More from "../More/More";
 
-function MoviesCardList({isSaved}) {
+function MoviesCardList({ isSaved }) {
     const [isWidth, setIsWidth] = useState(window.innerWidth);
-    const [isAddCountMovies, setAddCountMovies] = useState(addCountMovies(isWidth));
-    const [isCountMovies, setIsCountMovies] = useState(getInitialCountMovies(isWidth));
+    const [isInitialCountMovies, setIsInitialCountMovies] = useState(
+        getInitialCountMovies(isWidth)
+    );
+    const [isAddCountMovies, setAddCountMovies] = useState(
+        addCountMovies(isWidth)
+    );
+    const [isCountMovies, setIsCountMovies] = useState(
+        getInitialCountMovies(isWidth)
+    );
     const [isMore, setIsMore] = useState(true);
 
     useEffect(() => {
-        window.addEventListener('resize', function() {
+        window.addEventListener("resize", function () {
             setTimeout(() => {
                 setIsWidth(window.innerWidth);
             }, 300);
         });
 
-        setAddCountMovies(addCountMovies(isWidth))
-        countMoreMovies()
-    }, [])
+        setIsInitialCountMovies(getInitialCountMovies(isWidth));
+        setAddCountMovies(addCountMovies(isWidth));
+        countMoreMovies();
 
+    }, [isWidth]);
+
+    useEffect(() => {
+        setIsCountMovies(isInitialCountMovies);
+        countMoreMovies();
+    }, [isInitialCountMovies, isAddCountMovies]);
 
     function getInitialCountMovies(width) {
         if (width > 900) {
-            return 12
+            return 12;
         } else if (width <= 900 && width > 550) {
-            return 8
+            return 8;
         } else if (width <= 550) {
-            return 5
+            return 5;
         }
     }
 
     function addCountMovies(width) {
         if (width > 900) {
-            return 3
-        } else return 2
+            return 3;
+        } else return 2;
     }
 
     function addMoreMovies() {
-        console.log(isAddCountMovies)
-        setIsCountMovies(prevCount => prevCount + isAddCountMovies);
+        console.log(isAddCountMovies);
+        setIsCountMovies((prevCount) => prevCount + isAddCountMovies);
     }
 
     function countMoreMovies() {
-        if (isCountMovies >= movies.length) setIsMore(false)
+        if (isCountMovies >= movies.length) setIsMore(false);
     }
 
     useEffect(() => {
-        countMoreMovies()
-    }, [isCountMovies])
-
+        countMoreMovies();
+    }, [isCountMovies]);
 
     return (
         <>
-            <section className={`moviesCardList ${isSaved ? "moviesCardList__saved" : ''}`}>
+            <section
+                className={`moviesCardList ${
+                    isSaved ? "moviesCardList__saved" : ""
+                }`}
+            >
                 {movies.slice(0, isCountMovies).map((movie, index) => {
-                    return <MovieCard movie={movie} key={index}/>;
+                    return <MovieCard movie={movie} key={index} />;
                 })}
             </section>
-            {!isSaved && isMore && <More handleaddMoreMovies = {addMoreMovies}/>}
+            {!isSaved && isMore && <More handleaddMoreMovies={addMoreMovies} />}
         </>
     );
 }
