27a08b5c3515839a494c468fd0a636837ab04a67
Your Name <mash.pogonina2014@yandex.ru>
2023-08-30 23:58:34 +0300

    feat: добавить редактирование данных в профиле

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -11,68 +11,6 @@ export function useForm(inputValues) {
     return { values, handleChange, setValues };
 }
 
-// export function useFormWithValidation() {
-//     const [values, setValues] = useState({});
-//     const [errors, setErrors] = useState({});
-//     const [isValid, setIsValid] = useState(false);
-
-//     const handleChange = (event) => {
-//         const target = event.target;
-//         const name = target.name;
-//         const value = target.value;
-
-//         setValues({ ...values, [name]: value });
-
-//         console.log('values: ', values, "value: ", value, "isValid: ", isValid)
-
-//         if (name === "name") {
-//             const nameRegex = /^[A-Za-zА-Яа-яЁё\s-]*$/;
-//             const isNameValid = nameRegex.test(value);
-//             const isLengthValid = value.length >= 2 && value.length <= 30;
-
-//             let errorMessage = "";
-
-//             if (!isNameValid) {
-//                 errorMessage =
-//                     "Имя должно содержать только латиницу, кириллицу, пробел или дефис";
-//                 if (!isLengthValid) {
-//                     if (value.length < 2)
-//                         errorMessage += ` и быть длиной от 2 символов, сейчас ${value.length}`;
-//                     if (value.length > 30)
-//                         errorMessage += ` и быть длиной до 30 символов, сейчас ${value.length}`;
-//                 }
-//             } else if (isNameValid && !isLengthValid) {
-//                 errorMessage = `Имя должно быть длиной от 2 до 30 символов, сейчас ${value.length}`;
-//             }
-
-//             setErrors((prevErrors) => ({
-//                 ...prevErrors,
-//                 [name]: errorMessage,
-//             }));
-//         } else if (name === "email") {
-//             const emailRegex =
-//                 /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-//             const isEmailValid = emailRegex.test(value);
-
-//             setErrors((prevErrors) => ({
-//                 ...prevErrors,
-//                 [name]: isEmailValid ? "" : "Email не соответствует формату",
-//             }));
-//         } else {
-//             setErrors((prevErrors) => ({
-//                 ...prevErrors,
-//                 [name]: target.validationMessage,
-//             }));
-//             setIsValid(target.closest("form").checkValidity());
-//         }
-
-//         setIsValid(target.closest("form").checkValidity());
-//     };
-
-//     useEffect(() => {
-//         setIsValid(Object.values(errors).every((error) => error === ""));
-//     }, [errors]);
-
 export function useFormWithValidation() {
     const [values, setValues] = useState({});
     const [errors, setErrors] = useState({});
